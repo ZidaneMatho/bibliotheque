4228675bb1f5fbc75ad843cfea4e171faddeeeba
ZidaneMatho <mathozidane@gmail.com>
2026-03-07 00:28:05 +0100

    Ajout du frontend Angular

---
+++ .angular/cache/21.2.1/bibliotheque-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-XKLRU5KI.js";
+import "./chunk-RDJSAKSN.js";
+import "./chunk-XLSPPSYU.js";
+import "./chunk-E6HQ2WRY.js";
+import "./chunk-ESUXTPJQ.js";
+import "./chunk-H5KNGSDP.js";
+import "./chunk-ROCU7VXU.js";
+import "./chunk-C27DBZK2.js";
+import "./chunk-2UVUUPPC.js";
+import "./chunk-K54IFBYX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
